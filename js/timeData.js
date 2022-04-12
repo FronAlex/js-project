afb4d87de5ef25d3811af5e5200d2a79b9aaae8d
alex <albabenka@gmail.com>
2022-04-12 23:27:48 +0300

    getDate

--- js/timeData.js
+++ js/timeData.js
@@ -1,43 +1,33 @@
 
-function zero_first_format(value) {
+function format(value) {
     if (value < 10) {
-        value = '0' + value;
-    }
-    return value;
-}
+        value = '0'
+        return value;
+    };
+};
 
-
-function date_time() {
-    var current_datetime = new Date();
-    var day = zero_first_format(current_datetime.getDate());
-    var month = current_datetime.getMonth() + 1;
-    var year = current_datetime.getFullYear();
-    var hours = zero_first_format(current_datetime.getHours());
-    var minutes = zero_first_format(current_datetime.getMinutes());
-    var seconds = zero_first_format(current_datetime.getSeconds());
+function time() {
+    var datetime = new Date();
+    var day = format(datetime.getDate());
+    var month = datetime.getMonth() + 1;
+    var year = datetime.getFullYear();
+    var hours = format(datetime.getHours());
+    var minutes = format(datetime.getMinutes());
+    var seconds = format(datetime.getSeconds());
     var spanJS = document.querySelector('.spanJS');
     var months = { 1: "января", 2: "февраля", 3: "Марта", 4: "апреля", 5: "мая", 6: "июня", 7: "июля", 8: "августа", 9: "сентября", 10: "октября", 11: "ноября", 12: "декабря", };
     spanJS.innerHTML = '';
     spanJS.innerHTML = `${day} ${months[month]} ${year}`
 
     return hours + ":" + minutes + ":" + seconds;
-}
+};
 
 
-document.querySelector('.timeJS').innerHTML = date_time();
+document.querySelector('.timeJS').innerHTML = time();
 
 setInterval(function () {
-    document.querySelector('.timeJS').innerHTML = date_time();
+    document.querySelector('.timeJS').innerHTML = time();
 }, 1000);
-
-
-
-
-
-
-
-
-
 
 
 
